426e1e67130d1f2ed03628b876ea850b8b2bff41
UNSW account <unswaccount@anujans-air-2.home>
2024-02-29 20:39:00 +1100

    made the smile bigger

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express()
 app.listen(5001, () => console.log('API running on port 5001'));
 
 app.get('/', (req, res) => {
-    res.json('My API running :)');
+    res.json('My API running :D');
 });
