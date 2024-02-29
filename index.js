31558ed27d5b6c26d62e13f95da8add8866a2380
UNSW account <unswaccount@anujans-air-2.home>
2024-02-29 20:50:15 +1100

    got the file-download route done

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ const app = express()
 app.listen(5001, () => console.log('API running on port 5001'));
 
 app.get('/', (req, res) => {
-    res.json('My API running :D');
+    return res.json('My API running :D');
 });
+
+app.get('/chicken', (req, res) => {
+    return res.download("./hello.jpeg", (err) => {
+        if (err) {
+            res.send({
+                error : err,
+                msg   : "Problem downloading the file"
+            })
+        }});
+})
